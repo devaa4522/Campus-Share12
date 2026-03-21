6ce83d1253195d2043741b0140f2419cd1703c95
Devendra Palli <devendrapalli143@gmail.com>
2026-03-21 23:08:02 +0530

    Commit - 6

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,4 +1,4 @@
-giimport { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import TasksClient from "@/components/TasksClient";
 
